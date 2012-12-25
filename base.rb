5af4e48561b1479d333687b7313d2f5f855ceda3
René Kersten <rene.kersten@gmail.com>
2012-12-26 00:14:42 +0100

    add project.yml

--- base.rb
+++ base.rb
@@ -4,9 +4,6 @@ gem_group :development, :test do
 end
 generate :rspec
 
-# init git repo
-git :init
-
 # add database example
 run "cp config/database.yml config/example_database.yml"
 
@@ -20,13 +17,31 @@ gsub_file "config/application.rb", "# config.time_zone = 'Central Time (US & Can
 # cleanup unnecessary files
 run "rm .gitignore public/index.html app/assets/images/rails.png"
 
+# add project.yml config
+run "wget -O config/example_project.yml https://raw.github.com/endorfin/rails-templates/master/files/example_project.yml"
+run "cp config/example_project.yml config/project.yml"
+inject_into_file 'config/application.rb', after: "require 'rails/all'\n" do <<-'RUBY'
+
+CONFIG = YAML.load(File.read(File.expand_path('../project.yml', __FILE__)))
+CONFIG.merge! CONFIG.fetch(Rails.env, {})
+CONFIG.symbolize_keys!
+RUBY
+end
+
+# init git repo
+git :init
+
 # gitignore
 file ".gitignore", <<-END
 log/*.log
 tmp/**/*
 config/database.yml
+config/project.yml
 db/*.sqlite3
 END
 
 # initial commit
 git :add => ".", :commit => "-m 'initial commit'"
+
+
+
